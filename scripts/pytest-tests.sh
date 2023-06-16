4245a9d766906670e31f47c5ba267212a15d8d4c
Tiago Castro <tiagolobocastro@gmail.com>
2023-06-16 15:02:04 +0100

    test: test ka
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- scripts/pytest-tests.sh
+++ scripts/pytest-tests.sh
@@ -26,7 +26,7 @@ function run_tests()
     (
       set -x
       base=$(dirname "$name")
-      python -m pytest --tc-file='test_config.ini' --docker-compose="$base" "$name"
+      python -m pytest --tc-file='test_config.ini' --docker-compose="$base" "$name" -svv
     )
     fi
   done
@@ -42,23 +42,6 @@ cd "$SRCDIR/test/python" && source ./venv/bin/activate
 
 run_tests << 'END'
 
-tests/replica
-tests/publish
-tests/rebuild
-
-# tests/csi
-
-tests/ana_client
-tests/cli_controller
-tests/replica_uuid
-# tests/rpc
-
-tests/nexus_multipath
-tests/nexus_fault
-tests/nexus
-
-v1/pool
-v1/replica
-v1/nexus
+tests/nexus/test_multi_nexus_ka.py
 
 END
